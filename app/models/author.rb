bcd84fd540c5af3c2555f86fef964a83e6ce7949
OtienohOwinoh <coowinoh@gmail.com>
2022-10-30 21:52:19 +0300

    Complete Challenge

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,42 @@
+require_relative 'article'
+
+
 class Author
-  attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
     @name = name
+    @articles = []
+    @magazine = []
+
   
+  end
+
+  def articles
+    @articles
+
+  end
+
+  def magazines
+     @magazines.uniq
+
+  end
+
+  def add_article(magazine, title)
+    @articles << Article.new(self, magazine, title)
+    @magazines << magazine
+
+  end
+
+  def topic_areas
+    magazines = @magazines.map do |magazine|
+      magazine.category
+
+    end
+
+    magazines.uniq
+
   end
 
 
